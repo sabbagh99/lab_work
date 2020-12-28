add052b3bf722379533a76dd2e6ab6c51a84889a
Ahmad AlSabbagh <a.sameer1999@gmail.com>
2020-12-28 15:28:48 +0200

    adding other question

--- class_02/lab_02.js
+++ class_02/lab_02.js
@@ -1,13 +1,63 @@
 'use strict'
 alert ('Hello in about me page, I will give you a quick quiz about me answer with (yes/no)')
-var Q1 = prompt('Do you think my age is 32? ')
-var Q2 = 
-var Q3 = 
-var Q4 = 
-var Q5 = 
-if(Q1.toLocaleLowerCase === 'yes'){
-alert ('No its actualy 22')
-}else{
-    alert('Yes you are right its 22')
+var Q1 = prompt('Do you think my age is 32? ');
+
+switch (Q1.toLocaleLowerCase()){
+    case 'yes':
+    case 'y':
+        alert('No its actualy 22');
+        break;
+        case 'no':
+        case 'n':
+        alert ('Yes you are right its 22');
+        break;
 }
 
+// var Q2 = prompt('My fovarate color is green?');
+// switch (Q2.toLocaleLowerCase()){
+//     case 'yes':
+//     case 'y':
+//         alert ('No its actualy yallow');
+//         break;
+//         case 'no':
+//         case 'n':
+//         alert ('Yes you are right its yallow');
+//         break;
+// }
+// var Q3 = prompt('Do you think iam living in amman?');
+// switch (Q3.toLocaleLowerCase()){
+//     case 'yes':
+//     case 'y':
+//         alert ('Yes you are right');
+//         break;
+//     case 'no':
+//     case 'n':
+//         alert('No its actualy i live in amman');
+//         break;
+// }
+// var Q4 = prompt('My fovaraite food is burgar?');
+// switch (Q4.toLocaleLowerCase()){
+//     case 'yes':
+//     case 'y':
+//         alert('No its actualy shawarma');
+//         break;
+//         case'no':
+//     case 'n':
+//         alert('Yes you are right its shawarma');
+//         break;
+// }
+// var Q5 = prompt('Do you think iam Married');
+// switch (Q5.toLocaleLowerCase()){
+//     case 'yes':
+//     case 'y':
+//         alert('No iam not ');
+//         break;
+//     case'no':
+//     case 'n':
+//         alert('Yes you are right')
+//         break;
+// }
+// console.log('Yes you are correct');
+// console.log('Yes you are not correct');
+    alert('Yes you are correct');
+    alert('Yes you are not correct');
