73a7dfc721a5675021d1e165a9d70ac9762f1141
Ahmad AlSabbagh <a.sameer1999@gmail.com>
2020-12-28 14:07:20 +0200

    add quastion_1

---
+++ class_02/lab_02.js
@@ -0,0 +1,13 @@
+'use strict'
+alert ('Hello in about me page, I will give you a quick quiz about me answer with (yes/no)')
+var Q1 = prompt('Do you think my age is 32? ')
+var Q2 = 
+var Q3 = 
+var Q4 = 
+var Q5 = 
+if(Q1.toLocaleLowerCase === 'yes'){
+alert ('No its actualy 22')
+}else{
+    alert('Yes you are right its 22')
+}
+
